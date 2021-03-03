cac5ef6bc984fabf04008f492fa2fc36a3a2a9ab
Roberto dos Santos Filho <robertodossantosfilho1001@gmail.com>
2021-03-03 19:44:00 -0400

    Componente modal

---
+++ src/components/levelUpModal.tsx
@@ -0,0 +1,19 @@
+import { useContext } from 'react'
+import { ChallengesContexts } from '../contexts/ChallengeContexts'
+import styles from '../styles/components/levelupmodal.module.css'
+
+export function LevelUpModal() {
+    const {level, closeLevelUpModal} = useContext(ChallengesContexts)
+   return(
+    <div className={styles.overlay}>
+        <div className={styles.container}>
+            <header>{level}</header>
+                <strong>Parabéns</strong>
+                <p>Você alcançou um novo level.</p>
+                <button type="button" onClick={closeLevelUpModal}>
+                    <img src="/icons/close.svg" alt="Fecha Modal"/>
+                </button>
+        </div>
+    </div>
+   )
+}
