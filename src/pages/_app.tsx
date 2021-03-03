3ffa98b715ccd23160ecc91b661e6fac7def9537
Roberto dos Santos Filho <robertodossantosfilho1001@gmail.com>
2021-03-03 19:40:05 -0400

    Retirou os provider da aplicaçao

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,9 @@
-import { ChallengesProvider } from '../contexts/ChallengeContexts'
+
 import '../styles/global.css'
 
 function MyApp({ Component, pageProps }): JSX.Element {
   return(
-    <ChallengesProvider>
        <Component {...pageProps} />
-    </ChallengesProvider>
   )
 }
 
